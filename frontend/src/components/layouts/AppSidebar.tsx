b30b74e77cf393bfb153b206520cd0dd188f3b3b
jhphon0730 <eeung025@gmail.com>
2024-04-26 10:37:18 +0900

    * Sidebar - Sidebar Design

--- frontend/src/components/layouts/AppSidebar.tsx
+++ frontend/src/components/layouts/AppSidebar.tsx
@@ -1,8 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import { SidebarList } from "../../constants/sidebar";
 
 const AppSidebar = () => {
+  const location = useLocation()
+
+  console.log(location.pathname);
+  
+
   return (
     <div className="hidden lg:flex fixed left-0 top-0 w-40 h-full p-2 flex-col items-center bg-gray-700 text-white">
       <div className="text-center mb-4">
@@ -13,7 +18,7 @@ const AppSidebar = () => {
       <div className="text-center flex-1 w-full">
         <ul className="flex flex-col justify-center items-center gap-2 w-full">
           { SidebarList.map((sidebarItem) => (
-            <li key={sidebarItem.content} className="w-full p-2 bg-gray-600 rounded-md">
+            <li key={sidebarItem.content} className={`w-full p-2 rounded-md ${ location.pathname == sidebarItem.path ? "bg-gray-800 font-bold" : "bg-gray-600" }`}>
               <Link to={sidebarItem.path} className="p-2">{ sidebarItem.content}</Link>
             </li>
           ))}
