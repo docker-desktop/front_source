496a4a49bd266463dea06c644f7c8556052e4969
jhphon0730 <eeung025@gmail.com>
2024-05-23 14:11:12 +0900

    Add Get-Docker-Version-Service

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainPage = () => {
 	return (
-		<div className="w-full h-full">
+		<div className="w-full h-full ">
 			<h1 className="text-2xl">Docker Desktop Info</h1>
 		</div>
 	);
--- pkg/services/program_service.go
+++ pkg/services/program_service.go
@@ -20,3 +20,12 @@ func NewProgramService(docker_client client.Client) *programService {
 		docker_client: docker_client,
 	}
 }
+
+func (p *programService)DockerInfo() (types.VersionInfo, error) {
+	docker_version, err := p.docker_client.GetVersion(*p.ctx)
+	if err != nil {
+		return types.VersionInfo{}, err
+	}
+
+	return docker_version, nil
+}
