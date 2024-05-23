a3735434991aa27ef17ab6a6db72f837909d17cc
jhphon0730 <eeung025@gmail.com>
2024-05-23 11:37:13 +0900

    Add Main Page Comment - Update DockerFetcher Library Version

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const MainPage = () => {
-  return (
-    <div>
-      Main Page
-    </div>
-  );
+	return (
+		<div className="w-full h-full">
+			<h1 className="text-2xl">Docker Desktop Info</h1>
+		</div>
+	);
 };
 
 export default MainPage;
